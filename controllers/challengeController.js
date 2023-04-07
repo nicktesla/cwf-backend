e18f849b57a2b96bb5e12b69a117de73153f18f1
Debo Olaosebikan <debo@Debos-MacBook-Pro.local>
2023-04-07 16:21:11 -0400

    fixed broken challenges controller'

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -10,4 +10,31 @@ exports.createChallenge = async (req, res) => {
     res.json(newChallenge.rows[0]);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ error:
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
+};
+
+exports.getChallengeById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const challenge = await pool.query("SELECT * FROM challenges WHERE id = $1", [id]);
+    if (challenge.rowCount === 0) {
+      res.status(404).json({ error: "Challenge not found" });
+    } else {
+      res.json(challenge.rows[0]);
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
+};
+
+exports.getAllChallenges = async (req, res) => {
+  try {
+    const challenges = await pool.query("SELECT * FROM challenges");
+    res.json(challenges.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "An internal server error occurred" });
+  }
+};
